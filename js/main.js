f0b68cb9f7f9d4ebc571973e7adaaa1af8237d85
Ayodhya Sankalpa <ayodhyasankalpa2@gmail.com>
2026-05-11 06:00:50 +0530

    create a blog page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const observer = new IntersectionObserver((entries) => {
+window.observer = new IntersectionObserver((entries) => {
     entries.forEach((e, i) => {
       if (e.isIntersecting) {
         setTimeout(() => e.target.classList.add('visible'), i * 80);
       }
     });
   }, { threshold: 0.1 });
-  document.querySelectorAll('.fade-up').forEach(el => observer.observe(el));
+  document.querySelectorAll('.fade-up').forEach(el => window.observer.observe(el));
 
   document.querySelectorAll('a[href^="#"]').forEach(a => {
     a.addEventListener('click', e => {
